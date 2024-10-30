97bd0894a880edbbbbe7a8fa71cfad699cdd36f9
luke-dauner <luke.dauner@duke.edu>
2024-10-30 12:15:46 -0400

    Committ of new argos script

--- ImportArgos.py
+++ ImportArgos.py
@@ -6,7 +6,7 @@
 ##
 ## Usage: ImportArgos <ARGOS folder> <Output feature class> 
 ##
-## Created: Fall 2023
+## Created: Fall 2024
 ## Author: John.Fay@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
 
@@ -17,14 +17,14 @@ import sys, os, arcpy
 inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
 
-# Construct a while loop to iterate through all lines in the datafile
-# Open the ARGOS data file for reading
+# Construct a while loop and iterate through all lines in the data file
+# Open the ARGOS data file
 inputFileObj = open(inputFile,'r')
 
-# Get the first line of data, so we can use a while loop
+# Get the first line of data, so we can use the while loop
 lineString = inputFileObj.readline()
 
-# Start the while loop
+#Start the while loop
 while lineString:
     
     # Set code to run only if the line contains the string "Date: "
@@ -45,13 +45,17 @@ while lineString:
         # Extract the date we need to variables
         obsLat = line2Data[2]
         obsLon= line2Data[5]
+                    
+        # Extract the date, time, and LC values
+        obsDate = lineData[3]
+        obsTime = lineData[4]
+        obsLC   = lineData[7]
         
         # Print results to see how we're doing
-        print (tagID,"Lat:"+obsLat,"Long:"+obsLon)
+        print (tagID,"Lat:"+obsLat,"Long:"+obsLon, obsLC, obsDate, obsTime)
         
     # Move to the next line so the while loop progresses
     lineString = inputFileObj.readline()
     
 #Close the file object
 inputFileObj.close()
-
